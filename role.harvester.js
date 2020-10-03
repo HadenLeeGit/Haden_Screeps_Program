8321015cfb7c0522f5ad420bff6b1ed8fa7f5d66
HadenLeeGit <188708@gmail.com>
2020-10-03 03:18:56 -0400

    Version 0.3.0

--- role.harvester.js
+++ role.harvester.js
@@ -28,8 +28,7 @@ var roleHarvester = {
         else { //delivery energy to STRUCTURE
             var targets = creep.room.find(FIND_STRUCTURES, {
                     filter: (structure) => {
-                        return ((structure.structureType == STRUCTURE_EXTENSION 
-                            && structure.store.getFreeCapacity(RESOURCE_ENERGY) == 0)
+                        return (structure.structureType == STRUCTURE_EXTENSION 
                             || structure.structureType == STRUCTURE_SPAWN);
                     }
             });
